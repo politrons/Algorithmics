020d42f2cabc6ad22bc142a372286236d9f6df4f
Pablo.Perez.Garcia <pablo.perez.garcia@greenvalleybelgium.be>
2015-09-03 11:10:51 +0200

    bubble algorithm

---
+++ src/test/java/BubbleSortTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * @author Pablo Perez
+ */
+public class BubbleSortTest {
+
+
+    @Test
+    public void descendingSort() {
+        int[] numbers = {5, 3, 6, 1, 2};
+        int swap;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int k = 0; k < numbers.length - 1; k++) {
+                if (numbers[k] > numbers[k + 1]) {
+                    swap = numbers[k];
+                    numbers[k] = numbers[k + 1];
+                    numbers[k + 1] = swap;
+                }
+            }
+        }
+        assertThat("[1,2,3,5,6]", is(Arrays.toString(numbers).trim()));
+
+    }
+
+
+}
